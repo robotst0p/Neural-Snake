9518fa851e840105659b1beb7ffac08c58485bad
Tyler Meyer <meyertyler68@gmail.com>
2022-04-13 12:22:38 -0400

    Finished Tail, altered movement, fixed food hitbox, changed randomization

--- grid.py
+++ grid.py
@@ -3,7 +3,6 @@ import random
 import time
 
 from pygame.locals import * 
-from input_handler import *
 from snake_class import *
 
 SIZE = 1000, 1000
@@ -12,6 +11,7 @@ GRAY = (150, 150, 150)
 WHITE = (255, 255, 255)
 
 key_dict = {K_DOWN: (0, 10), K_UP: (0, -10), K_LEFT: (-10, 0), K_RIGHT: (10, 0)}
+snake_body = []
 
 pygame.init()
 screen = pygame.display.set_mode(SIZE) 
@@ -23,11 +23,11 @@ pygame.display.set_caption("Neural Snake")
 key = (0,0)
 
 def randomize():
-    snake_tile_x = random.randint(0, 1000)
-    snake_tile_y = random.randint(0, 1000)
+    snake_tile_x = random.randrange(0, 600, 10)
+    snake_tile_y = random.randrange(0, 600, 10)
 
-    food_tile_x = random.randint(0, 1000)
-    food_tile_y = random.randint(0, 1000)
+    food_tile_x = random.randrange(0, 600, 10)
+    food_tile_y = random.randrange(0, 600, 10)
 
     snake_head = snake_tile(snake_tile_x, snake_tile_y, 10, 10, (0,0))
     food_tile = Rect(food_tile_x, food_tile_y, 10, 10)
@@ -36,15 +36,71 @@ def randomize():
 
 snake_head, food_tile = randomize()
 
-def border_check(snake_head, food_tile):
+def border_check(snake_head, food_tile, snake_body):
+    for tile in snake_body:
+        if snake_head.x == tile.x and snake_head.y == tile.y:
+            snake_head, food_tile = randomize()
+            snake_body = []
+            
     if snake_head.x >= 1000 or snake_head.y >= 1000 or snake_head.x <= 0 or snake_head.y <= 0:
         snake_head, food_tile = randomize()
+        snake_body = []
 
-    return snake_head, food_tile
+    return snake_head, food_tile, snake_body
 
+def food_check(snake_head, food_tile, snake_body):
+    if len(snake_body) > 0:
+        last_tile = snake_body[0]
+    elif len(snake_body) == 0:
+        last_tile = snake_head
+
+    if snake_head.x == food_tile.x and snake_head.y == food_tile.y:
+        food_tile.x = random.randrange(0, 600, 10)
+        food_tile.y = random.randrange(0, 600, 10)
+
+        if last_tile.direction == (10, 0):
+            new_tile = snake_tile(last_tile.x - 10, last_tile.y, 10, 10, (0,0))
+            snake_body.insert(0,new_tile)
+        elif last_tile.direction == (-10, 0):
+            new_tile = snake_tile(last_tile.x + 10, last_tile.y, 10, 10, (0,0))
+            snake_body.insert(0, new_tile)
+           
+        elif last_tile.direction == (0, 10):
+            new_tile = snake_tile(last_tile.x, last_tile.y - 10, 10, 10, (0,0))
+            snake_body.insert(0, new_tile)
+           
+        elif last_tile.direction == (0, -10):
+            new_tile = snake_tile(last_tile.x, last_tile.y + 10, 10, 10, (0,0))
+            snake_body.insert(0, new_tile)
+    return snake_body
+
+def dir_update(snake_body, snake_head):
+    work_arr = []
+    print(snake_body)
+    if len(snake_body) > 0:
+        snake_body[-1].direction = snake_head.direction
+    for i in range(0, len(snake_body) - 1):
+        print(snake_body[i].direction)
+        if snake_body[i + 1].x > snake_body[i].x:
+            snake_body[i].direction = (10, 0)
+        elif snake_body[i + 1].x < snake_body[i].x:
+            snake_body[i].direction = (-10, 0)
+        elif snake_body[i + 1].y < snake_body[i].y:
+            snake_body[i].direction = (0, -10)
+        elif snake_body[i + 1].y > snake_body[i].y:
+            snake_body[i].direction = (0, 10)
+        print(snake_body[i].direction)
+        work_arr.append(snake_body[i])
+    
+    if len(snake_body) > 0:
+        work_arr.append(snake_body[-1])
+
+    snake_body = work_arr
+    return snake_body
 
 #render loop
 while running:
+    loop_body = []
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
@@ -52,15 +108,28 @@ while running:
     screen.fill(GRAY)
     pygame.draw.rect(screen, RED, food_tile)
     pygame.draw.rect(screen, WHITE, snake_head)
+
+    for tile in snake_body:
+        pygame.draw.rect(screen, WHITE, tile)
+
     pygame.display.flip()
 
     if event.type == KEYDOWN:
         key = key_dict[event.key]
-        snake_head.direction = key
+        if key[0] is not -snake_head.direction[0] or key[1] is not -snake_head.direction[1]:
+            snake_head.direction = key
     
-    snake_head = snake_head.move(key[0], key[1])
-    snake_head, food_tile = border_check(snake_head, food_tile)
-    time.sleep(.05)
+    snake_head = snake_head.move(snake_head.direction[0], snake_head.direction[1])
+   
+    for tile in snake_body:
+        tile = tile.move(tile.direction[0], tile.direction[1])
+        loop_body.append(tile)
+    snake_body = loop_body
+   
+    snake_head, food_tile, snake_body = border_check(snake_head, food_tile, snake_body)
+    snake_body = food_check(snake_head, food_tile, snake_body)
 
+    snake_body = dir_update(snake_body, snake_head)
+    time.sleep(.05)
 
 pygame.quit()
--- snake_class.py
+++ snake_class.py
@@ -2,6 +2,11 @@ import pygame
 
 class snake_tile(pygame.Rect):
     def __init__(self, left, top, width, height, direction):
-        self.direction = direction 
         super().__init__(left, top, width, height)
+        self.direction = direction
+
+    def move(self, x_offset, y_offset):
+        new_instance = super().move(x_offset, y_offset)
+        new_instance.direction = self.direction
+        return new_instance
 
