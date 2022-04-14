91190a1ff88f9a0a7b80cbd2459a3e6e28d9e119
imagine_orange <76490538+ImagineOrange@users.noreply.github.com>
2022-04-14 00:29:53 -0400

    Delete snake_class.py

--- snake_class.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-class snake_tile(pygame.Rect):
-    def __init__(self, left, top, width, height, direction):
-        self.direction = direction 
-        super().__init__(left, top, width, height)
-
